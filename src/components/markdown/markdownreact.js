3fdda3612ec56974d98eaa8e1aed2834cda2ca60
Ranita Ghosh <ranitaghosh04@gmail.com>
2024-05-14 00:34:48 +0530

    commiting

--- src/components/markdown/markdownreact.js
+++ src/components/markdown/markdownreact.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import './style.css'
+import React, { useState } from "react";
+import "./style.css";
+import ReactMarkdown from 'react-markdown'
 
 const Markdownreact = () => {
+  const [markDown, setMarkDown] = useState("Welcome to Markdown");
   return (
     <>
-    <div className='center-div'>
-        <textarea className='left-side'></textarea>
-        <div className='right-side'></div>
-    </div>
+      <div className="center-div">
+        <textarea
+          className="left-side"
+          value={markDown}
+          onChange={(e) => setMarkDown(e.target.value)}
+        ></textarea>
+        <div className="right-side">
+            <ReactMarkdown>{markDown}</ReactMarkdown>
+            </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Markdownreact
+export default Markdownreact;
