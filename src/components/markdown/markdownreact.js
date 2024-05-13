49adaf5b77c21b1fa77e2bb17af5cab1364a8ce6
Ranita Ghosh <ranitaghosh04@gmail.com>
2024-05-13 23:52:22 +0530

    first commit

---
+++ src/components/markdown/markdownreact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+const Markdownreact = () => {
+  return (
+    <>
+    <div className='center-div'>
+        <textarea className='left-side'></textarea>
+        <div className='right-side'></div>
+    </div>
+    </>
+  )
+}
+
+export default Markdownreact
